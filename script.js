f5df385104c1303a406f9af8b2c5422a14ca03cc
Shubham Kumar <kumaruikeshubham@gmail.com>
2025-08-25 20:21:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function copyText(text) {
+  navigator.clipboard.writeText(text).then(() => toast('Copied!'));
+}
+
+function copyFromElement(id) {
+  const el = document.getElementById(id);
+  if (el) copyText(el.innerText);
+}
+
+function toast(msg) {
+  const t = document[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/Teknokasi/batch-1/tree/def6705585ff1eae6fcd1c6cf8730cacb8f844e0/week-1%2Fday-5%2F8-html-basic-7.md?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "1")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/lzh-yi/Web-Fork-/tree/024b3e55587afdf9f05a677613a75f24e3d1803e/03-CSS%E8%BF%9B%E9%98%B6%2F04-%E5%A6%82%E4%BD%95%E8%AE%A9%E4%B8%80%E4%B8%AA%E5%85%83%E7%B4%A0%E6%B0%B4%E5%B9%B3%E5%9E%82%E7%9B%B4%E5%B1%85%E4%B8%AD%EF%BC%9F.md?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "2")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/umarashraf71/School-Security-and-Information-Management-System/tree/3ab9ed4800209e9cd99fdcda0192d3fe3b152c88/admin%2Fadmin-login.php?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "3")
